0b3a329e4e87e579919fbfa3ded9f17809ddb498
PouplarWesel <94089618+PouplarWesel@users.noreply.github.com>
2023-03-21 18:18:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 import random
 
 def boxExp(diceAmount, boxXAmount, boxYAmount, trialNum):
-    #create a list of numbers in ranoage of 1 to 6, and the amount of numbers in the list is the dice amount
-    diceListX = []
-    diceListY = []
-    boxXCount = diceAmount
-    boxYCount = 0
-    for x in range(trialNum):
-        diceListX = []
-        for x in range(boxXCount):
-            diceListX.append(random.randint(1, 6))
-        #count how many numbers are equal to or less than the boxXAmount
-        for x in diceListX:
-            if x <= boxXAmount:
-                boxYCount += 1
-                boxXCount -= 1
-        #now do the same with Y
-        diceListY = []
-        for x in range(boxYCount):
-            diceListY.append(random.randint(1, 6))
-        for x in diceListY:
-            if x <= boxYAmount:
-                boxXCount += 1
-                boxYCount -= 1
-    print("The amount of dice in each box is " + str(boxXCount) + " and " + str(boxYCount) + " respectively.")
+    # open a file for writing the results
+    with open("experiment_results.txt", "w") as f:
+        # write the headers to the file
+        f.write("Data Table — Equilibrium Trial 1\n")
+        f.write("{:>10s}{:>20s}{:>35s}{:>20s}{:>40s}\n".format("",
+            "Reactants (Successful Forward = 1)", "", "Products (Successful Reverse = 6)", ""))
+        f.write("{:>10s}{:>20s}{:>35s}{:>20s}{:>40s}\n".format("Throw", "Starting Dice",
+            "Number of Successful Forward Reactions", "Starting Dice",
+            "Number of Successful Reverse Reactions"))
+        f.write("\n")
 
+        # initialize variables
+        boxXCount = diceAmount
+        boxYCount = 0
 
-    
-boxExp(1000, 1, 5, 1000)
+        # do the trials and write the results to the file
+        for throw in range(1, trialNum+1):
+            backUp = ("The amount of dice in each box is {} and {} respectively.".format(boxXCount, boxYCount))
+            # roll the dice for X box
+            diceListX = [random.randint(1, 6) for i in range(boxXCount)]
+            # count the number of successful forward reactions
+            forwardCount = sum(1 for d in diceListX if d <= boxXAmount)
+
+            # roll the dice for Y box
+            diceListY = [random.randint(1, 6) for i in range(boxYCount)]
+            # count the number of successful reverse reactions
+            reverseCount = sum(1 for d in diceListY if d <= boxYAmount)
+
+            # write the results to the file
+            f.write("{:>10d}{:>20d}{:>35d}{:>20d}{:>40d}\n".format(throw, boxXCount,
+                forwardCount, boxYCount, reverseCount))
+
+            # update the counts for next trial
+            boxXCount -= forwardCount
+            boxYCount += forwardCount
+            boxYCount -= reverseCount
+            boxXCount += reverseCount
+            
+        # write the final box counts to the file
+        f.write("\n")
+        f.write(backUp)
+
+# run the experiment with given parameters
+boxExp(100, 1, 1, 1000)
